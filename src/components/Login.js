11b5bc4f51496d6bf97e1b46c43e99f6173bd33d
Meshack <mbuvi.meshack99@gmail.com>
2023-03-26 21:10:24 +0300

    password input

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,26 +10,30 @@ const [password, setPassword] = useState ("")
             
                 <h1 class="text-2xl font-light">LOGIN</h1> <br />
                 <h2 class="text-center">Sign in to your account</h2>
-                <form class="mt-4 bg-white shadow-md rounded-lg">
+                <form class="mt-4 bg-white shadow-md rounded-lg sm">
                 <div class="h-2 bg-indigo-400 rounded-t-md"></div>
                 <div class="px-8 py-6"></div>
                 <label class="block font-semibold">Username</label>
                 <input 
-                class="border  h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"
+                class="border  h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-400 rounded-md"
                 type="text"
                 placeholder="Name"
                 value={number}
                 onChange={(e => setNumber(e.target.value))}
-                /> <br />
-
+                />
+                 <label class="block mt-3 font-semibold">Password</label>
                 <input 
-                className=""
+                className="border  h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-400 rounded-md"
                 type="text"
                 placeholder="Password"
                 value={password}
                 onChange={(e => setPassword(e.target.value))}
                 />
-                
+                <div class="flex justify-between items-baseline">
+                    <button type="submit" class="mt-4 bg-indigo-500 text-white py-2 px-6 rounded-md hover:bg-indigo-600">Login</button>
+                    <a href="#" class="text-sm hover:underline">Forgot password ? </a>
+
+                </div>
                 
             </form>
 
