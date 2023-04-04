f88fd510338aac0b3a5716db3226af0645691c32
Meshack <mbuvi.meshack99@gmail.com>
2023-04-05 00:47:21 +0300

    new files and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Login from './components/Login';
+import Heading from './components/Heading';
+import Subheading from './components/Subheading';
 
 function App() {
   return (
-    <div class="bg-green-500 h-screen">
+    <div class="bg-green-600 h-screen">
+      <Heading />
      <Login />
+     <Subheading />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,16 +5,17 @@ import kinatwa from "./kinatwa.png"
 function Login () {
 const [number, setNumber] = useState ("")
 const [password, setPassword] = useState ("")
+
     
     return(
-        <div class="justify-center">
+        <div class="flex justify-center ">
             
                
-                <form class="p-mt-4 bg-white rounded-lg sm:w-96 mx-auto text-center ">
+                <form class=" mt-12  bg-white rounded-lg sm:w-96 text-center p-8 ">
                 <img src={kinatwa} class=" mx-auto" alt=""/>
                 {/* <div class="h-2 bg-indigo-400 rounded-t-md"></div> */}
                 <h1 class="text-4xl text-black font-serif">LOGIN</h1> <br />
-                <h2 class="text-center text-2xl font-thin">Sign in to your account</h2>
+                <h2 class="text-center text-2xl font-thin">Sign in to your account</h2> <br/>
                 {/* <div class="px-8 py-6"></div> */}
                 <label class="block font-semibold">Phone Number</label>
                 <input 
@@ -28,18 +29,20 @@ const [password, setPassword] = useState ("")
                  <label class="block mt-3 font-semibold">Password</label>
                 <input 
                 className="border  h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-400 rounded-md"
-                type="text"
+                type="password"
                 placeholder="Password"
                 value={password}
                 onChange={(e => setPassword(e.target.value))}
                 />
-                <div class="flex justify-between items-baseline">
-                    <button type="submit" class="mt-4 bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600">Login</button>
+                <div class="">
+                    <button type="submit" class="mt-8 bg-blue-500 text-white py-2 px-16 rounded-full hover:bg-blue-700">Sign In</button>
                     
 
                 </div>
                 
             </form>
+
+           
 
 
         </div>
